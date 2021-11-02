888cf8e6edd582647fdaa7660fe6f02d54bcfb70
shankalpaLamichhane <shankalpa.lamichhane@gmail.com>
2021-11-02 16:57:33 +0545

    commenting db connection for test

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -17,27 +16,32 @@ var dbConfig struct {
 
 func main() {
 
-	dns := fmt.Sprintf("host=%s port=5432 user=%s password=%s dbname=%s sslmode=require",
-		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+	// dns := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=require",
+	// 	os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_USER"),
+	// 	os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
 
 	log.Printf("THE DATABASE URL IS ::: ", os.Getenv("POSTGRES_HOST"))
 	// dns := os.Getenv("DATABASE_URL")
 
-	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
-	if err != nil {
-		log.Printf("ERROR IN CONNECTION ::::::: ", err.Error())
-		panic(err)
-	}
+	// db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
+	// if err != nil {
+	// 	log.Printf("ERROR IN CONNECTION ::::::: ", err.Error())
+	// 	panic(err)
+	// }
 	router := gin.Default()
 	router.GET("/v1/tasks", func(c *gin.Context) {
 		var tasks []models.Task
-		db.First(&tasks)
+		fmt.Print(tasks)
+		// db.First(&tasks)
 		c.JSON(200, gin.H{
 			"status":  "ok",
 			"message": "success",
-			"data":    tasks,
+			"data":    "tasks",
 		})
 	})
-	router.Run(":" + os.Getenv("SERVER_PORT"))
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	router.Run(":" + port)
 }

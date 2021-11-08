f783ad3fb7a3ffed3b58378330d66815465f0604
shankalpaLamichhane <shankalpa.lamichhane@gmail.com>
2021-11-08 11:31:27 +0545

    fixed errors

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"github-actions-project/models"
 	"log"
 	"os"
 
@@ -30,8 +28,6 @@ func main() {
 	// }
 	router := gin.Default()
 	router.GET("/v1/tasks", func(c *gin.Context) {
-		var tasks []models.Task
-		fmt.Print(tasks)
 		// db.First(&tasks)
 		c.JSON(200, gin.H{
 			"status":  "ok",
